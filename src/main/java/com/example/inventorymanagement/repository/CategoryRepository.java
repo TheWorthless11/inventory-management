bf178ccd742870070256230c11aae8685c7baa8a
Mahhia Mim <mahhiamim@gmail.com>
2026-03-13 22:55:00 +0600

    feat: add product productdetail and  category repositories

---
+++ src/main/java/com/example/inventorymanagement/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.example.inventorymanagement.repository;
+
+import com.example.inventorymanagement.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> { /* table name(Category), primary column type(Long)*/
+}
+/* We are copying all the pre-written database code from Spring Boot into this file.
+JpaRepository': This is the Spring Boot "Magic Wand".
+         * It secretly contains all the SQL queries for save(), findAll(), findById(), delete(), etc.
+ */
