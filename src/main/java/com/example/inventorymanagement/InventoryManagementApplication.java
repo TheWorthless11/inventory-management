1fe62f35cb8728b6e1922801fa919d8d26e3eb56
Mahhia Mim <mahhiamim@gmail.com>
2026-03-13 22:33:47 +0600

    feat: add 6 core entities and commented database test code

--- src/main/java/com/example/inventorymanagement/InventoryManagementApplication.java
+++ src/main/java/com/example/inventorymanagement/InventoryManagementApplication.java
@@ -1,7 +1,11 @@
 package com.example.inventorymanagement;
 
+import com.example.inventorymanagement.entity.Category;
+import com.example.inventorymanagement.repository.CategoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryManagementApplication {
@@ -10,6 +14,22 @@ public class InventoryManagementApplication {
         SpringApplication.run(InventoryManagementApplication.class, args);
 
     }
+    // THIS IS THE TEST DRIVE CODE:
+//    @Bean
+//    public CommandLineRunner testDatabase(CategoryRepository categoryRepository){
+//        return args -> {
+//            // 1. Create a new Category
+//            Category myCategory = new Category();
+//            myCategory.setName("Electronics");
+//
+//            // 2. Save it to PostgreSQL
+//            categoryRepository.save(myCategory);
+//
+//            // 3. Print a success message
+//            System.out.println("TEST SUCCESSFUL: Saved "+myCategory.getName()+" to the database!");
+//        };
+//
+//    }
 
 
 
