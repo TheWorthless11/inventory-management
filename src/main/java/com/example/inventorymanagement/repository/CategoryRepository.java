a376722bb63b89380c0af3beaa2d0120da3df95d
tasnim_nz <nuzhattasnim800@gmail.com>
2026-03-24 01:48:59 +0600

    fixed integration test

--- src/main/java/com/example/inventorymanagement/repository/CategoryRepository.java
+++ src/main/java/com/example/inventorymanagement/repository/CategoryRepository.java
@@ -4,8 +4,11 @@ import com.example.inventorymanagement.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> { /* table name(Category), primary column type(Long)*/
+    Optional<Category> findByName(String name);
 }
 /* We are copying all the pre-written database code from Spring Boot into this file.
 JpaRepository': This is the Spring Boot "Magic Wand".
